6694cb32136d6d745605349193c4eebe3dfcd19a
miroslavmatovic <v-mirosm@microsoft.com>
2020-12-08 15:12:42 +0100

    replaced app to FirstApp

--- README.md
+++
@@ -1,2 +0,0 @@
-# ReactNativeApp
-React Native for Android test
--- ios/FirstAppTests/FirstAppTests.m
+++ ios/FirstAppTests/FirstAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeAppTests : XCTestCase
+@interface FirstAppTests : XCTestCase
 
 @end
 
-@implementation ReactNativeAppTests
+@implementation FirstAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
